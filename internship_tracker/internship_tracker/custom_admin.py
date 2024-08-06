2c6f423a8246b6c6a47bc9677e07e5b67dbc8e9c
Praisesomiji <praise@gmail.com>
2024-08-07 00:50:08 +0100

    Create a custom admin site for interns

---
+++ internship_tracker/internship_tracker/custom_admin.py
@@ -0,0 +1,10 @@
+from django.contrib.admin import AdminSite
+from django.utils.translation import ugettext_lazy as _
+
+class InternAdminSite(AdminSite):
+    site_header = _("Intern - Internship Tracker")
+    site_title = _("Intern Site UI")
+    index_title = _("Welcome to the Internship Tracker")
+
+intern_admin_site = InternAdminSite(name='intern')
+
