cf4598f5bbcf998ccf14159e0a72a19aeb6f49ab
Praisesomiji <praise@gmail.com>
2024-08-07 00:47:43 +0100

    Debug and Test Phase 1 of this project

--- internship_tracker/activities/admin.py
+++ internship_tracker/activities/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Tag, Activity, Report
+from .models import Tag, Location, Activity, Report
 
-for app in (Tag, Activity, Report,):
+for app in (Tag, Location, Activity, Report,):
     admin.site.register(app)
--- internship_tracker/planner/models.py
+++ internship_tracker/planner/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User, Group
 from products.models import Product
+from datetime import timedelta
 
 class Week(models.Model):
     title = models.CharField(max_length=100)
