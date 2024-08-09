43f24e2641fb5647416f59088fe25245b2001387
Praisesomiji <praise@gmail.com>
2024-08-09 21:20:29 +0100

    Add workshop and details to the vents app

--- internship_tracker/events/models.py
+++ internship_tracker/events/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Details(models.Model):
+    date = models.DateField()
+    time = models.TimeField()
+    venue = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.date} at {self.time} - {self.venue}"
+
+class Workshop(models.Model):
+    title = models.CharField(max_length=255)
+    about = models.TextField()
+    details = models.ManyToManyField(Details, related_name="workshops")
+
+    def __str__(self):
+        return self.title
