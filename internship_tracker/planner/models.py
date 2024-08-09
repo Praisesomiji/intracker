2905754c35511b04972556cab909560d4a70e019
Praisesomiji <praise@gmail.com>
2024-08-09 21:33:38 +0100

    Add a new model called Documents to the planner app

--- internship_tracker/planner/models.py
+++ internship_tracker/planner/models.py
@@ -3,6 +3,14 @@ from django.contrib.auth.models import User, Group
 from products.models import Product
 from datetime import timedelta
 
+class Document(models.Model):
+    title = models.CharField(max_length=255)
+    file = models.FileField(upload_to='documents/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
 class Week(models.Model):
     title = models.CharField(max_length=100)
     start_date = models.DateField()
